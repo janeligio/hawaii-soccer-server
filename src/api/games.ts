7daffb5531cb68071ba475761a1a9acb3c7bf3a4
Jan <jeligio808@gmail.com>
2021-05-05 20:48:57 -1000

    Add email notification functionality

--- src/api/games.ts
+++ src/api/games.ts
@@ -1,9 +1,22 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 import { Game } from '../models/Game';
+import { Venue } from '../models/Venue';
 
-router.get('/')
+import { MAIL_USERNAME, MAIL_PASS, clientID, clientSecret, refreshToken } from '../config/keys';
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: MAIL_USERNAME,
+        pass: MAIL_PASS,
+        clientId: clientID,
+        clientSecret: clientSecret,
+        refreshToken: refreshToken
+    }
+})
 /**
  * /api/v1/ GET - Get all local soccer games
  * /api/v1/ POST - Add a local soccer game
@@ -47,15 +60,34 @@ router.post('/create', (req, res) => {
 
 router.get('/join', (req, res) => {
     const gameId = req.query.id;
-    const { email, playerCount } = req.body;
+    const { email, name, playerCount } = req.body;
 
     Game.findById(gameId, (err, game) => {
         if(err) {
             res.status(500).json(err);
         } else {
-            console.log(game);
-            game.playerCount = game.playerCount + playerCount;
-            game.save();
+            // Email the organizer
+            Venue.findById(game.venue, (err, venue) => {
+                const mailOptions = {
+                    from: MAIL_USERNAME,
+                    to: game.email,
+                    subject: `Hawaii Five-a-Side Notification: Person has joined your game!`,
+                    text: `${name} has joined your game at ${venue.name} from ${game.startTime}-${game.endTime}. Contact them at ${email}.`
+                }
+
+                transporter.sendMail(mailOptions, (err, data) => {
+                    if (err) {
+                        console.log("Error sending mail: " + err);
+                        res.status(500).send("Error sending mail");
+                    } else {
+                        console.log("Successfully notified organizer");
+                        // Update playercount of the game document
+                        game.playerCount = game.playerCount + (playerCount || 1);
+                        game.save();
+                        res.status(200).send("Email notification sent.");
+                    }
+                })
+            });
         }
     });
 })
